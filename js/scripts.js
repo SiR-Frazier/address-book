2d8ecc9c6b74280e9a48d4eca20b0033d0deea60
bryce frazier and frank ngo <bryce@>
2018-01-22 08:31:05 -0800

    update html and js to include address and email for each contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 //back-end logic
-function Contact(first, last) {
+function Contact(first, last, address, email) {
   this.firstName = first;
   this.lastName = last;
+  this.address = address;
+  this.email = email;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
 
 //user-end logic
@@ -11,13 +17,17 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedAddress = $("input#address").val();
+    var inputtedEmail = $("input#email").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedAddress,inputtedEmail);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#address").val();
+    $("input#email").val();
   });
 
   $(".contact").last().click(function() {
